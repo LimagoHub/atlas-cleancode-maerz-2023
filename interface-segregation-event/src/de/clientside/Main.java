d2f38006ee182e61b99c4879e07d61aaa9b16c65
Joachim Wagner <jowagner@limago.de>
2023-03-26 11:44:35 +0200

    initial commit

---
+++ interface-segregation-event/src/de/clientside/Main.java
@@ -0,0 +1,29 @@
+package de.clientside;
+
+import de.tiere.PigTooFatListener;
+import de.tiere.Schwein;
+
+
+
+public class Main {
+
+
+
+	public static void main(String[] args) {
+		new Main().run();
+
+	}
+
+	private void run() {
+
+
+
+	}
+
+
+
+
+}
+
+
+
